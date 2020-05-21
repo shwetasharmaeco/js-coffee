3c0732e33a08f05289f051d09f4bb7fed3c20a86
Shweta Sharma <shwetasharmaeco@gmail.com>
2020-05-21 08:35:21 +0000

    completed

---
+++ static/js/coffeeShop.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const addItemToCart = (itemName) => {
+  $('#cart-items').append(`
+    <tr>
+      <td>${itemName}</td>
+    </tr>
+  `);
+};
+
+$('.add-to-order').on('click',() =>{
+ addItemToCart('coffee');
+incrementCartTotal(1.50);
+});
+
+
+const resetCart = () => {
+  $('#cart-total').html('0.00');
+  $('#cart-items').empty();
+};
+
+const incrementCartTotal = (price) => {
+  const cartTotal = $('#cart-total');
+
+  let total = Number(cartTotal.html());
+  total += price;
+
+  cartTotal.html(total.toFixed(2));
+};
+
+const incrementCoffeeSold = (amountSold) => {
+  let coffeeSold = Number($('#coffee-sold-counter').html());
+  coffeeSold += amountSold;
+
+  $('#coffee-sold-counter').html(coffeeSold);
+};
+
+const setProgressAndStatus = (progressVal, statusMsg) => {
+  $('#order-progress').attr('value', progressVal);
+  $('#order-status-message').html(statusMsg);
+};
+$('#place-order').on('click', () => {
+  incrementCoffeeSold($('#cart-items').children().length);
+   resetCart();});
+
+//
+// Add your event handlers below.
+//
